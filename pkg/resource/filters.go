79e6acfe19afa3d7b435259658256e1d0b6ba806
lishijun10 <lishjun01@126.com>
2021-10-26 01:42:40 +0800

    add plugin filter

--- pkg/resource/filters.go
+++ pkg/resource/filters.go
@@ -1,12 +1,17 @@
 package resource
 
 import (
+	"time"
+
+	"github.com/golang/protobuf/ptypes/wrappers"
+
 	core "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	listener "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
 	cors "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/cors/v3"
 	fault "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/fault/v3"
 	grpcstats "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/grpc_stats/v3"
 	grpcweb "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/grpc_web/v3"
+	lua "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/lua/v3"
 	router "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/router/v3"
 	httpwasm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/wasm/v3"
 	httpinspector "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/listener/http_inspector/v3"
@@ -41,39 +46,10 @@ type RouterFilterContext struct {
 // Define static filters to be reused across the codebase. This avoids duplicate marshaling/unmarshaling
 // This should not be used for filters that will be mutated
 var (
-	Cors = &hcm.HttpFilter{
-		Name: wellknown.CORS,
-		ConfigType: &hcm.HttpFilter_TypedConfig{
-			TypedConfig: MessageToAny(&cors.Cors{}),
-		},
-	}
-	Fault = &hcm.HttpFilter{
-		Name: wellknown.Fault,
-		ConfigType: &hcm.HttpFilter_TypedConfig{
-			TypedConfig: MessageToAny(&fault.HTTPFault{}),
-		},
-	}
 	Router = &hcm.HttpFilter{
 		Name: wellknown.Router,
 		ConfigType: &hcm.HttpFilter_TypedConfig{
 			TypedConfig: MessageToAny(&router.Router{}),
-		},
-	}
-	GrpcWeb = &hcm.HttpFilter{
-		Name: wellknown.GRPCWeb,
-		ConfigType: &hcm.HttpFilter_TypedConfig{
-			TypedConfig: MessageToAny(&grpcweb.GrpcWeb{}),
-		},
-	}
-	GrpcStats = &hcm.HttpFilter{
-		Name: wellknown.HTTPGRPCStats,
-		ConfigType: &hcm.HttpFilter_TypedConfig{
-			TypedConfig: MessageToAny(&grpcstats.FilterConfig{
-				EmitFilterState: true,
-				PerMethodStatSpecifier: &grpcstats.FilterConfig_StatsForAllMethods{
-					StatsForAllMethods: &wrapperspb.BoolValue{Value: false},
-				},
-			}),
 		},
 	}
 	TLSInspector = &listener.ListenerFilter{
@@ -228,4 +204,273 @@ func MessageToAny(msg proto.Message) *anypb.Any {
 		return nil
 	}
 	return out
+}
+
+type ListenerFilter interface {
+	GetName() string
+	Build() *listener.ListenerFilter
+}
+
+type HttpFilter interface {
+	GetName() string
+	Build() *hcm.HttpFilter
+}
+
+type WASMFilter struct {
+	Name     string
+	FilePath string
+	RootId   string
+	FailOpen bool
+}
+
+func (f *WASMFilter) GetName() string {
+	return f.Name
+}
+
+func (f *WASMFilter) Build() *hcm.HttpFilter {
+	httpMxConfigProto := &httpwasm.Wasm{
+		Config: &wasm.PluginConfig{
+			Vm: ConstructVMConfig(f.FilePath, "envoy.wasm.metadata_exchange"),
+			//TODO wasm plugin configuration support
+			//Configuration: MessageToAny(&metadata_exchange.MetadataExchange{}),
+			Name:     f.Name,
+			RootId:   f.RootId,
+			FailOpen: f.FailOpen,
+		},
+	}
+	return &hcm.HttpFilter{
+		Name:       f.Name,
+		ConfigType: &hcm.HttpFilter_TypedConfig{TypedConfig: MessageToAny(httpMxConfigProto)},
+	}
+}
+
+type LuaFilter struct {
+	InlineCode    string
+	LuaDataSource []LuaDataSource
+}
+
+type LuaDataSource struct {
+	Name           string
+	DataSourceType string
+	Value          string
+}
+
+func (f *LuaFilter) GetName() string {
+	return wellknown.Lua
+}
+
+func (f *LuaFilter) Build() *hcm.HttpFilter {
+	var sourceCodes = map[string]*core.DataSource{}
+	for _, item := range f.LuaDataSource {
+		switch item.DataSourceType {
+		case "Filename":
+			sourceCodes[item.Name] = &core.DataSource{
+				Specifier: &core.DataSource_Filename{
+					Filename: item.Value,
+				},
+			}
+		case "InlineBytes":
+			sourceCodes[item.Name] = &core.DataSource{
+				Specifier: &core.DataSource_InlineBytes{
+					InlineBytes: []byte(item.Value),
+				},
+			}
+		case "InlineString":
+			sourceCodes[item.Name] = &core.DataSource{
+				Specifier: &core.DataSource_InlineString{
+					InlineString: item.Value,
+				},
+			}
+		}
+	}
+	return &hcm.HttpFilter{
+		Name: wellknown.Lua,
+		ConfigType: &hcm.HttpFilter_TypedConfig{
+			TypedConfig: MessageToAny(&lua.Lua{
+				InlineCode:  f.InlineCode,
+				SourceCodes: sourceCodes,
+			}),
+		},
+	}
+}
+
+type GrpcStatsFilter struct {
+}
+
+func (f *GrpcStatsFilter) GetName() string {
+	return wellknown.HTTPGRPCStats
+}
+
+func (f *GrpcStatsFilter) Build() *hcm.HttpFilter {
+	return &hcm.HttpFilter{
+		Name: wellknown.HTTPGRPCStats,
+		ConfigType: &hcm.HttpFilter_TypedConfig{
+			TypedConfig: MessageToAny(&grpcstats.FilterConfig{
+				EmitFilterState: true,
+				PerMethodStatSpecifier: &grpcstats.FilterConfig_StatsForAllMethods{
+					StatsForAllMethods: &wrapperspb.BoolValue{Value: false},
+				},
+			}),
+		},
+	}
+}
+
+type GrpcWebFilter struct {
+}
+
+func (f *GrpcWebFilter) GetName() string {
+	return wellknown.GRPCWeb
+}
+
+func (f *GrpcWebFilter) Build() *hcm.HttpFilter {
+	return &hcm.HttpFilter{
+		Name: wellknown.GRPCWeb,
+		ConfigType: &hcm.HttpFilter_TypedConfig{
+			TypedConfig: MessageToAny(&grpcweb.GrpcWeb{}),
+		},
+	}
+}
+
+//RouterFilter this is all default filter
+type RouterFilter struct {
+	// Specifies a list of HTTP headers to strictly validate. Envoy will reject a
+	// request and respond with HTTP status 400 if the request contains an invalid
+	// value for any of the headers listed in this field. Strict header checking
+	// is only supported for the following headers:
+	//
+	// Value must be a ','-delimited list (i.e. no spaces) of supported retry
+	// policy values:
+	//
+	// * :ref:`config_http_filters_router_x-envoy-retry-grpc-on`
+	// * :ref:`config_http_filters_router_x-envoy-retry-on`
+	//
+	// Value must be an integer:
+	//
+	// * :ref:`config_http_filters_router_x-envoy-max-retries`
+	// * :ref:`config_http_filters_router_x-envoy-upstream-rq-timeout-ms`
+	// * :ref:`config_http_filters_router_x-envoy-upstream-rq-per-try-timeout-ms`
+	StrictCheckHeaders []string
+	// Defaults to true
+	DynamicStats             bool
+	StartChildSpan           bool
+	UpstreamLog              *AccessLog
+	SuppressEnvoyHeaders     bool
+	RespectExpectedRqTimeout bool
+}
+
+type AccessLog struct {
+	Name string
+	// TODO this build have some problem
+	// StatusCodeFilter
+	FilterType string
+	// EQ 0
+	// GE 1
+	// LE 2
+	Op int
+	// AccessLog_TypedConfig or AccessLog_HiddenEnvoyDeprecatedConfig
+	ConfigType string
+}
+
+func (f *RouterFilter) GetName() string {
+	return wellknown.Router
+}
+
+func (f *RouterFilter) Build() *hcm.HttpFilter {
+	var dynamicStats = true
+	if !f.DynamicStats {
+		dynamicStats = f.DynamicStats
+	}
+	//TODO AccessLog AND StrictCheckHeaders logic
+	return &hcm.HttpFilter{
+		Name: wellknown.Router,
+		ConfigType: &hcm.HttpFilter_TypedConfig{
+			TypedConfig: MessageToAny(&router.Router{
+				DynamicStats:             &wrappers.BoolValue{Value: dynamicStats},
+				StrictCheckHeaders:       f.StrictCheckHeaders,
+				StartChildSpan:           f.StartChildSpan,
+				SuppressEnvoyHeaders:     f.SuppressEnvoyHeaders,
+				RespectExpectedRqTimeout: f.RespectExpectedRqTimeout,
+			}),
+		},
+	}
+}
+
+type FaultFilter struct {
+	Delay FaultDelay
+	Abort FaultAbort
+
+	UpstreamCluster                 string
+	Headers                         HeaderRoute
+	DownstreamNodes                 []string
+	MaxActiveFaults                 *uint32
+	ResponseRateLimit               *FaultRateLimit
+	DelayPercentRuntime             string
+	AbortPercentRuntime             string
+	DelayDurationRuntime            string
+	AbortHttpStatusRuntime          string
+	MaxActiveFaultsRuntime          string
+	ResponseRateLimitPercentRuntime string
+	AbortGrpcStatusRuntime          string
+	DisableDownstreamClusterStats   bool
+}
+type FaultRateLimit struct {
+	// FixedLimit or HeaderLimit
+	LimitType string
+
+	// if type is FixedLimit use this
+	LimitKbps uint64
+
+	Percentage uint32
+	// 1/100 = 1%. 0
+	// 1/10000 = 0.01%. 1
+	// 1/1000000 = 0.0001%. 2
+	DenominatorType int
+}
+
+type FaultAbort struct {
+	// HttpStatus or GrpcStatus or HeaderAbort
+	ErrorType string
+	// if type is httpStatus is set value to httpStatus if GrpcStatus setValue to GrpcStatus if HeaderAbort ignore
+	Status uint32
+
+	Percentage uint32
+	// 1/100 = 1%. 0
+	// 1/10000 = 0.01%. 1
+	// 1/1000000 = 0.0001%. 2
+	DenominatorType int
+}
+type FaultDelay struct {
+	//FixedDelay or HeaderDelay
+	Type       string
+	FixedDelay time.Duration
+}
+
+func (f *FaultFilter) GetName() string {
+	return wellknown.Fault
+}
+
+func (f *FaultFilter) Build() *hcm.HttpFilter {
+	//TODO build fault.HTTPFault instance from FaultFilter
+	return &hcm.HttpFilter{
+		Name: wellknown.Fault,
+		ConfigType: &hcm.HttpFilter_TypedConfig{
+			TypedConfig: MessageToAny(&fault.HTTPFault{}),
+		},
+	}
+}
+
+type CORSFilter struct {
+}
+
+func (f *CORSFilter) GetName() string {
+	return wellknown.CORS
+}
+
+func (f *CORSFilter) Build() *hcm.HttpFilter {
+	return &hcm.HttpFilter{
+		Name: wellknown.CORS,
+		ConfigType: &hcm.HttpFilter_TypedConfig{
+			TypedConfig: MessageToAny(&cors.Cors{}),
+		},
+	}
 }
